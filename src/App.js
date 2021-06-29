ab9a887290338ce6dea85b3227a3c1925043e97b
ignaciosein@gmail.com <ignaciosein@gmail.com>
2021-06-29 17:38:44 +0200

    todo funcional

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Main from './components/Main/Main';
 
 function App() {
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import List from '../List/List'  
+import "./Main.scss"
 export default class Main extends Component {
     render() {
         return (
-            <main>
+            <main className="Main">
+
+            <div className="wrapper">
                 <List/>
+                </div>
             </main>
         )
     }
